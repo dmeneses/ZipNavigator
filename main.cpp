c4b9015d3f5a1d527dc55eb88a0181613ca19088
Daniela Meneses <daniela11290@gmail.com>
2013-03-05 21:21:05 -0400

    Navigate searching file names in the local file headers.

--- main.cpp
+++ main.cpp
@@ -6,54 +6,62 @@
  */
 
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
+#include <sys/unistd.h> 
 typedef FILE* PFILE;
 const char* fileName = "Test.zip";
 
-void navigateLocalHeader() {
+void navigateLocalHeaderToGetNames() {
     PFILE file = fopen(fileName, "r+b");
-    
-    if (file != NULL) {
-        int a;
-        fread(&a, sizeof (a), 1, file);
-        printf("Signature: %d\n", a);
-        short b;
-        fread(&b, sizeof(b), 1, file);
-        printf("Version needed to extract : %d\n", b);
-        fread(&b, sizeof(b), 1, file);
-        printf("General purpose bit flag: %d\n", b);;
-        fread(&b, sizeof(b), 1, file);
-        printf("Compression method: %d\n", b);
-        fread(&b, sizeof(b), 1, file);
-        printf("File last modification time: %d\n", b);
-        fread(&b, sizeof(b), 1, file);
-        printf("File last modification date: %d\n", b);
-        fread(&a, sizeof (a), 1, file);
-        printf("CRC-32 : %d\n", a);
-        fread(&a, sizeof (a), 1, file);
-        printf("Compressed size: %d\n", a);
-        fread(&a, sizeof (a), 1, file);
-        printf("Uncompressed size: %d\n", a);
-        short c;
-        fread(&c, sizeof(c), 1, file);
-        printf("File name length (n): %d\n", c);
-        fread(&b, sizeof(b), 1, file);
-        printf("Extra field length (m): %d\n", b);
-        char name[c+1];
-        fread(&name, sizeof(char), c, file);
-        name[c]='\0';
-        printf("File name : %s\n", name);       
-    }
 
-    fclose(file);
-    file = 0;
+    short flag;
+    short compressionMethod;
+    short fileNameLength;
+    short extraFieldLength;
+    int compressedSize;
+    int uncompressedSize;
+    
+    while (!feof(file)) { // This condition needs to be changed to find the archive decryption header.
+        fseek(file, 6, SEEK_CUR); 
+        fread(&flag, sizeof (flag), 1, file);
+        if (flag != 3) { // if is equal to three there is a data descriptor.
+
+            fread(&compressionMethod, sizeof (compressionMethod), 1, file);
+
+            if (compressionMethod == 0 || (compressionMethod > 6 && compressionMethod < 11)) { // doesn't use compression at all.
+                fseek(file, 8, SEEK_CUR);
+                fread(&compressedSize, sizeof (compressedSize), 1, file);
+                fread(&uncompressedSize, sizeof (uncompressedSize), 1, file);
+                fread(&fileNameLength, sizeof (fileNameLength), 1, file);
+                fread(&extraFieldLength, sizeof (extraFieldLength), 1, file);
+
+                //Reading the name
+                char name[fileNameLength + 1];
+                fread(&name, sizeof (char), fileNameLength, file);
+                name[fileNameLength] = '\0';
+                printf("File name : %s\n", name);
+
+                if (extraFieldLength != 0) {
+                    char extraField[extraFieldLength];
+                    fread(&extraField, sizeof (extraField), 1, file);
+                    printf("Extra field: %s\n", extraField);
+                }
+
+                // Reading the file content.
+                char fileContent[uncompressedSize + 1];
+                fread(&fileContent, sizeof (char), uncompressedSize, file);
+                fileContent[uncompressedSize] = '\0';
+                printf("File content: %s\n", fileContent);
+            }
+        }
+    }
 }
 
 /*
  * 
  */
 int main() {
-    navigateLocalHeader();
+    navigateLocalHeaderToGetNames();
     return 0;
 }
 
